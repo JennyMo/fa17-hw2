6f8a6ec71e1db5133c0fbf74d461c05bb467cf90
Jenny Mo <jennym826@gmail.com>
2017-09-27 15:05:07 -0700

    Completed HW2

--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -1,12 +1,17 @@
 class Stringify
-  attr_accessor :name, :adjective
-
-  def initialize(name, adjective)
-    @name = name
-    @adjective = adjective
-  end
-
-  def what_am_i
-  	# YOUR IMPLEMENTATION HERE
-  end
+    attr_accessor :name, :adjective
+    
+    def initialize(name, adjective)
+        @name = name
+        @adjective = adjective
+    end
+    
+    def what_am_i
+        # YOUR IMPLEMENTATION HERE
+        if @name.blank? || @adjective.blank?
+            return "You are nothing!"
+            else
+            return @name + " is so " + @adjective
+        end
+    end
 end
